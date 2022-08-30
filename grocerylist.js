09a1afec6eefb46434c5d786fe933720f015dafd
Abd-Sobr <66172515+Abdoul-lab@users.noreply.github.com>
2022-08-30 12:02:43 +0000

    Update grocerylist.js

--- grocerylist.js
+++ grocerylist.js
@@ -4,7 +4,7 @@ $(function() {
 		var input = $('input[type=text][name=item]'),
 			value = input.val(),
 			need = ($(event.target).attr('id') === 'addNeed'),
-			item = $('<li><input type="checkbox" name="item"> ' + value + ' <a href="#">remove</a></li>'),
+			item = $('<li><input type="checkbox" name="item"> ' + value + ' <a href="#">X</a></li>'),
 			list = (need) ? $('ul').first() : $('ul').last();
 		
 		input.val("");
